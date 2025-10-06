a846bfaea198d6d57d8a7a1be8e4280320e4b70c
yxpjseo <yjins2004@gmail.com>
2025-10-06 22:12:11 +0900

    [정수론] 예제

---
+++ 03_정수론/예제/2960.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findPrime(int n, int k){
+    int cnt =0;
+    vector<bool> is_prime(n+1, true); //소수 여부 저장
+
+    for(int i=2; i<=n; i++){
+        if(!is_prime[i]){ //이미 지워진 수이면 탐색 x
+            continue;
+        }
+
+        for(int j=i; j<=n; j+=i){
+            if(! is_prime[j]){ //이미 지워진 수이면 탐색 x
+                continue;
+            }
+
+            is_prime[j]=false;
+
+            if(++cnt==k){
+                return j;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+
+    cout << findPrime(n,k) << "\n";
+
+    return 0;
+}
